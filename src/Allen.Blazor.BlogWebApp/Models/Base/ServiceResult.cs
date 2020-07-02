979cf756d9b33f5b60d21960a1230f129da32d98
yunqian44 <yunqian8@live.com>
2020-07-02 18:22:47 +0800

    add ServiceResult Model

---
+++ src/Allen.Blazor.BlogWebApp/Models/Base/ServiceResult.cs
@@ -0,0 +1,83 @@
+﻿using Allen.Blazor.BlogWebApp.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allen.Blazor.BlogWebApp.Models.Base
+{
+    /// <summary>
+    /// 服务层响应实体
+    /// </summary>
+    public class ServiceResult
+    {
+        /// <summary>
+        /// 本构造函数 默认为 处理成功
+        /// </summary>
+        public ServiceResult()
+        {
+
+        }
+    
+        /// <summary>
+        /// 本构造函数 默认为 处理成功，且将返回数据传入
+        /// </summary>
+        /// <param name="d">返回数据</param>
+        public ServiceResult(dynamic d)
+        {
+            this.Data = d;
+        }
+
+        /// <summary>
+        /// 默认作为出现错误时使用；
+        /// </summary>
+        /// <param name="code">错误码</param>
+        /// <param name="msg">错误信息</param>
+        public ServiceResult(ServiceResultCode code, string msg)
+        {
+            this.Code = ServiceResultCode.Failed;
+            this.Msg = msg;
+        }
+
+        /// <summary>
+        /// 成功
+        /// </summary>
+        public bool Success => Code == ServiceResultCode.Succeed;
+
+
+        /// <summary>
+        /// 响应码
+        /// </summary>
+        public ServiceResultCode Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public dynamic Data { get; set; }
+    }
+
+
+    /// <summary>
+    /// Api返回数据格式
+    /// </summary>
+    public class ServiceResult<T> : ServiceResult where T : class
+    {
+        public ServiceResult()
+        {
+            
+        }
+
+        public ServiceResult(T d)
+            : this()
+        {
+            this.Data = d;
+        }
+
+        public ServiceResult(ServiceResultCode code, string msg)
+        {
+            this.Code = ServiceResultCode.Failed;
+            this.Msg = msg;
+        }
+
+        public new T Data { get; set; }
+    }
+}
---
+++ src/Allen.Blazor.BlogWebApp/Models/Enum/ServiceResultCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allen.Blazor.BlogWebApp.Models.Enum
+{
+    /// <summary>
+    /// 服务层响应码枚举
+    /// </summary>
+    public enum ServiceResultCode
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        Succeed = 0,
+
+        /// <summary>
+        /// 失败
+        /// </summary>
+        Failed = 1,
+    }
+}

88933e9124d8e9d05e56dfdfe9fadab6ce90be90
yunqian44 <yunqian8@live.com>
2020-07-02 18:27:24 +0800

    AddSingleton Common add HttpClient

--- src/Allen.Blazor.BlogWebApp/Program.cs
+++ src/Allen.Blazor.BlogWebApp/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Allen.Blazor.BlogWebApp.Commons;
 
 namespace Allen.Blazor.BlogWebApp
 {
@@ -18,6 +19,8 @@ namespace Allen.Blazor.BlogWebApp
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton(typeof(Common));
 
             await builder.Build().RunAsync();
         }
